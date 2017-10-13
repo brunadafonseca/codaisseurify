7601dc9815bca9625556be99cccefdcc38ac73e2
brunadafonseca <brunadafonseca@gmail.com>
2017-10-13 23:20:51 +0200

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+lana_del_rey = Artist.create!(name: "Lana del Rey", genre: "Baroque pop, dream pop, trip hop, indie pop")
+darkside = Artist.create!(name: "Darkside", genre: "Electronica, experimental, downtempo, ambient")
+pink_floyd = Artist.create!(name: "Pink Floyd", genre: "Progressive rock, art rock, psychedelic rock")
+foreign_fields = Artist.create!(name: "Foreign Fields", genre: "Alternative, Indie")
+
+art_deco = Song.create!(name: "Art Deco", artist: lana_del_rey)
+dark_paradise = Song.create!(name: "Dark Paradise", artist: lana_del_rey)
+body_electric = Song.create!(name: "Body Electric", artist: lana_del_rey)
+
+paper_trails = Song.create!(name: "Paper Trails", artist: darkside)
+heart = Song.create!(name: "Paper Trails", artist: darkside)
+the_only_shrine_ive_seen = Song.create!(name: "The Only Shrine I've Seen", artist: darkside)
+
+time = Song.create!(name: "Time", artist: pink_floyd)
+high_hopes = Song.create!(name: "High Hopes", artist: pink_floyd)
+brain_damage = Song.create!(name: "Brain Damage", artist: pink_floyd)
+
+river_kings = Song.create!(name: "The River Kings", artist: foreign_fields)
+names_and_races = Song.create!(name: "Names and Races", artist: foreign_fields)
+little_lover = Song.create!(name: "Little Lover", artist: foreign_fields)
