994cce8fd4fd34b88f8a560c7c6a615d83ab0d51
brunadafonseca <brunadafonseca@gmail.com>
2017-10-15 01:04:02 +0200

    Test models: Artist

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "is invalid without name" do
+      artist = Artist.new(name: "")
+      artist.valid?
+      expect(artist.errors).to have_key(:name)
+    end
+
+    it "is invalid without genre" do
+      artist = Artist.new(genre: "")
+      artist.valid?
+      expect(artist.errors).to have_key(:genre)
+    end
+  end
 end
