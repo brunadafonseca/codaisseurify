55144f99ea8154fc94acd139f6e22d583424ca75
brunadafonseca <brunadafonseca@gmail.com>
2017-10-15 00:38:42 +0200

    Create artists' index page

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,69 @@
 class ArtistsController < ApplicationController
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+    @photos = @artist.photos
+    @songs = @artist.songs
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+    @photos = @artist.photos
+    @songs = @artist.songs
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+
+    if @artist.save
+      image_params.each do |image|
+        @artist.photos.create(image: image)
+      end
+
+      redirect_to @artist, notice: "Artist created"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    if @artist.update(artist_params)
+      image_params.each do |image|
+        @artist.photos.create(image: image)
+      end
+
+      redirect_to @artist, notice: "Artist updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    artist = Artist.find(params[:id])
+    artist.destroy
+
+    redirect_to artists_path
+  end
+
+  private
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+
+  def artist_params
+    params.require(:artist).permit(:name, :bio, :genre)
+  end
+
+  def image_params
+    params[:images].present? ? params.require(:images) : []
+  end
 end
