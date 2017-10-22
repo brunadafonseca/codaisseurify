dee104bbd5f1ceb67a736430e2f62283a21f109f
brunadafonseca <brunadafonseca@gmail.com>
2017-10-22 16:38:09 +0200

    Here's a jQuery version

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,104 +3,92 @@
 //= require jquery_ujs
 //= require_tree .
 
-var songsList = document.getElementById('songs-list');
-document.onload = appendEmptySongsListMsg();
+var songsList = $('#songs-list');
+var deleteButtons = $('.delete-button');
+var deleteAllSongsButton = $('#delete-all');
+var emptyListMsg = $('#empty-list');
 
-function initializeEventListeners() {
-  var deleteButtons = document.getElementsByClassName('delete-button');
-
-  for (var i = 0; i < deleteButtons.length; i++) {
-    deleteButtons[i].addEventListener('click', deleteSong);
-  }
-
-  var deleteAllSongsButton = document.getElementById('delete-all');
-  deleteAllSongsButton.addEventListener('click', deleteAllSongs);
-}
-
+toggleListStatus();
 initializeEventListeners();
 
-function createSong(title, id) {
-  var listItem = document.createElement('li');
-  listItem.innerHTML = title;
-  listItem.setAttribute('data-id', id);
-  songsList.appendChild(listItem);
+function addSongToDOM(title, id) {
+  var listItem = $('<li></li>');
+  listItem.html(title);
+  listItem.attr('data-id', id);
+  songsList.append(listItem);
 
-  var deleteButton = document.createElement('button');
-  deleteButton.innerHTML = 'x';
-  deleteButton.class = 'delete-button';
-  listItem.appendChild(deleteButton);
-  deleteButton.addEventListener('click', deleteSong);
+  var deleteButton = $('<button></button>');
+  deleteButton.html('x');
+  deleteButton.addClass('delete-button');
+  deleteButton.on('click', deleteSong);
+  listItem.append(deleteButton);
 }
 
 function submitSong(event) {
   event.preventDefault();
-  var inputField = document.getElementById('song-input');
-  var songTitle = inputField.value;
+  var inputField = $('#song-input');
+  var songTitle = inputField.val();
 
-  xhr = new XMLHttpRequest();
-  xhr.open('POST', `/api/artists/${artistId}/songs`);
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.onload = function(data) {
-    if (xhr.status !== 200) {
-      alert('Could not create new song');
-    } else {
-      var songId = JSON.parse(data.currentTarget.response).song.id;
-      createSong(songTitle, songId);
-      inputField.value = null;
+  $.ajax({
+    url: `/api/artists/${artistId}/songs`,
+    method: 'POST',
+    data: {
+      name: songTitle
     }
-  };
-
-  xhr.send(encodeURI('name=' + songTitle));
+  }).success(function (response) {
+    var songId = response.song.id;
+    addSongToDOM(songTitle, songId);
+    toggleListStatus();
+    inputField.val(null);
+  }).fail(function (response) {
+    alert("Could not create song.");
+  });
 }
 
-function appendEmptySongsListMsg() {
-  if ( songsList.innerHTML.trim() == "" ) {
-    var emptyListMsg = document.createElement('p');
-    emptyListMsg.id = 'empty-list';
-    emptyListMsg.innerHTML = 'This artist has no songs yet';
-    songsList.appendChild(emptyListMsg);
-  }
-}
-
-function deleteAllSongs() {
-  function removeSongsFromDOM() {
-    songsList.innerHTML = "";
-  }
-
-  xhr = new XMLHttpRequest();
-  xhr.open('DELETE', `/api/artists/${artistId}/songs`);
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.onload = function() {
-    if (xhr.status !== 200) {
-      alert('Could not delete songs');
-    } else {
-      removeSongsFromDOM();
-      appendEmptySongsListMsg();
-    }
-  };
-
-  xhr.send();
+function removeSongFromDOM(item) {
+  item.remove();
+  toggleListStatus();
 }
 
 function deleteSong(event) {
-  var listItem = event.target.parentElement;
-  function removeSongFromDOM() {
-    listItem.parentElement.removeChild(listItem);
-    appendEmptySongsListMsg();
+  var listItem = $(event.target.parentElement);
+  var songId = listItem.attr('data-id');
+
+  $.ajax({
+    url: `/api/artists/${artistId}/songs/${songId}`,
+    method: 'DELETE',
+  }).success(function () {
+    removeSongFromDOM(listItem);
+  }).fail(function () {
+    alert('Could not delete song.');
+  });
+}
+
+function removeSongsFromDOM() {
+  songsList.html("");
+}
+
+function deleteAllSongs() {
+  $.ajax({
+    url: `/api/artists/${artistId}/songs`,
+    method: 'DELETE',
+  }).success(function () {
+    removeSongsFromDOM();
+    toggleListStatus();
+  }).fail(function () {
+    alert('Could not delete songs.');
+  });
+}
+
+function toggleListStatus() {
+  if (songsList.html().trim() == "") {
+    emptyListMsg.show();
+  } else {
+    emptyListMsg.hide();
   }
+}
 
-  var songId = listItem.getAttribute('data-id');
-
-  xhr = new XMLHttpRequest();
-  xhr.open('DELETE', `/api/artists/${artistId}/songs/${songId}`);
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.onload = function() {
-    if (xhr.status !== 200) {
-      alert('Could not delete song');
-    } else {
-      removeSongFromDOM();
-    }
-  };
-
-  xhr.send();
+function initializeEventListeners() {
+  deleteButtons.on('click', deleteSong);
+  deleteAllSongsButton.on('click', deleteAllSongs);
 }
