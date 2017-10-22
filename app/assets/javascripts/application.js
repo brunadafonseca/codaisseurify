e421ae260df6134ee1eb8f70b87d9e9742d12044
brunadafonseca <brunadafonseca@gmail.com>
2017-10-22 02:04:48 +0200

    Implement AJAX - Add songs to artist

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,34 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require_tree .
+
+function createSong(title) {
+  var songsList = document.getElementById('songs-list');
+  var listItem = document.createElement("li");
+  listItem.innerHTML = title;
+  var deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "x";
+  deleteButton.class = "delete-button";
+  songsList.appendChild(listItem);
+  listItem.appendChild(deleteButton);
+}
+
+function submitSong(event) {
+  event.preventDefault();
+  var inputField = document.getElementById('song-input');
+  var songTitle = inputField.value;
+
+  xhr = new XMLHttpRequest();
+  xhr.open('POST', `/api/artists/${artistId}/songs`);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.onload = function() {
+    if (xhr.status !== 200) {
+      alert('Could not create new song');
+    } else {
+      createSong(songTitle);
+      inputField.value = null;
+    }
+  };
+
+  xhr.send(encodeURI('name=' + songTitle));
+}
